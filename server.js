ad85ce7f0766d48596c85cff636e6c11fd760341
Paul Marbach <fastfrwrd@gmail.com>
2013-04-28 18:30:49 -0500

    autoopen

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var connect = require('connect'),
-	colors = require('colors');
+	colors = require('colors'),
+    openn = require('open');
 
 connect.createServer(
     connect.static(__dirname)
@@ -11,4 +12,5 @@ console.log("================".grey);
 console.log("a play by ".green, "Bethany Corey".green.bold);
 console.log("music and player by ".green, "Paul Marbach\n".green.bold);
 console.log("to view, go to ".green, "http://localhost:8080".yellow.bold.underline);
-console.log("to close the server, hit ".green, "CTRL".red.bold, "+".magenta, "C".red.bold);
+
+openn('http://localhost:8080');
